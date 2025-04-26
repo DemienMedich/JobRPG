6cb5db047fc6bdc8783fe4f92fc65bb7e70db72e
DemienMedich <artdifferent1@gmailcom>
2025-04-26 13:44:37 +0300

    Setting1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include
-git checkout
-int main(); {
-    float version = 0,0,0,1;
-    int version = versioncontrol(version);
+#include <iostream>
+using namespace std;
+
+int main() {
+    float version = 0.1;
+    int versioncontrol(float version);
     cout << "Version: " << version << endl;
     return 0;
 }
