ac4fbbd00e6ff2a2930e4b57484272842bafa5ec
DemienMedich <artdifferent1@gmailcom>
2025-04-26 12:59:03 +0300

    Add main program and version control function; create MAKEFILE

---
+++ main.cpp
@@ -0,0 +1,12 @@
+int main(); {
+    float version = 0,0,0,1;
+    int version = versioncontrol(version);
+    cout << "Version: " << version << endl;
+    return 0;
+}
+int versioncontrol(float version) {
+    while (version < 1.0) {
+        version += 0.1;
+    }
+    return version;
+}
